d48c756bcc8a4ee7de17a2db3c15350898ce0608
Aurumdev952 <benjamugi20072@gmail.com>
2025-09-27 08:01:06 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# Replace placeholders in env-config.js with container env vars
 echo "Replacing environment variables in env-config.js"
+
 for var in BACKEND_URL; do
-  sed -i "s|REPLACE_ME|${!var}|g" /usr/share/nginx/html/env-config.js
+  eval value=\$$var
+  sed -i "s|REPLACE_${var}|$value|g" /usr/share/nginx/html/env-config.js
 done
 
 exec "$@"
